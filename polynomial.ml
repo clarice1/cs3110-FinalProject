68d37f7825e4493cc5757e7f78a301185a65180b
Clarice Pertel <cep87@cornell.edu>
2020-10-22 16:47:55 -0400

    Update functions in polynomial.ml with fold

--- polynomial.ml
+++ polynomial.ml
@@ -8,44 +8,43 @@ let zero = []
 
 let from_list lst = lst
 
-(** [poly_calc t z size] returns the value of term of degree [size]. *)
-let poly_calc t z size = 
-  Complex.pow z size |> Complex.mul t
-
-let eval p z = 
-  let (one : Complex.t) = {re = 1.; im = 0.} in
-  let size_p = float_of_int (List.length p - 1) in
-  let rec term p z size acc =
-    match p with 
-    | [] -> acc
-    | h :: t -> 
-      term t z (Complex.sub one size) (Complex.add acc (poly_calc h z size))
-  in term p z {re = size_p; im = 0.} {re = 0.; im = 0.}
+let eval p z =
+  let f acc c = Complex.add (Complex.mul acc z) c
+  in 
+  List.fold_left f Complex.zero p
 
 (** [bound_deg_l2 size p] returns infinity if [p] is of degree 1 or less. 
     It returns infinity if [p] does not diverge and it returns 0. if [p] 
     diverges immediately. *)
-let bound_deg_l2 size (p : Complex.t list)=
+let bound_deg_l2 size p =
   let norm h = Complex.norm h in
-  let not_1 (a : Complex.t) = 
-    a.re = -1. || a.im = 1. || a.im = -1. in 
   if size < 1 then infinity 
   else 
     match p with 
     | a :: b :: [] -> 
       if norm a < 1. then infinity else
       if norm a > 1. then 0. else (* note the case b/(1-a)*)
-      if not_1 a || norm b = 0. then infinity else 0.
+      if a <> Complex.one || norm b = 0. then infinity else 0.
     | _ -> failwith "broke if statement" 
 
-let get_bound (p : Complex.t list) = 
-  let size = List.length p - 1 in 
-  let rec form acc = function
-    | [] -> acc
-    | h :: [] -> acc /. (Complex.norm h)
-    | h :: t -> form (acc +. (Complex.norm h)) t
-  in 
-  if size >= 2 then Float.max 1. (form 1. (List.rev p)) else bound_deg_l2 size p
+(** [peek lst] returns the first element of [lst].
+    Returns: failwith "Empty" if [lst] is the empty list. *)
+let peek = function
+  | [] -> failwith "Empty"
+  | x :: _ -> Complex.norm x
+
+(** [pop lst] returns [lst] without its first element *)
+let pop = function
+  | [] -> []
+  | _ :: xs -> xs
+
+let get_bound p = 
+  let size = List.length p - 1 in
+  let f acc c = 
+    acc +. Complex.norm c
+  in
+  if size >= 2 then List.fold_left f 0. (pop p) /. peek p |> Float.max 1.
+  else bound_deg_l2 size p
 
 let bounded p z =
-  if get_bound p = infinity then Some (eval p z) else None
+  if Complex.norm z > get_bound p then None else Some (eval p z)
