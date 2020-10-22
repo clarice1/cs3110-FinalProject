bc5beacc63c35069e5101fd4f8b25e40d8d0618e
Pedro Suau <pps33@cornell.edu>
2020-10-22 18:17:59 -0400

    fixed colorize

--- toImage.ml
+++ toImage.ml
@@ -7,11 +7,9 @@ let colorize f m =
   let rows = rows m
   and columns = columns m in
   let orig_image = Rgb24.make rows columns {r = 0; g=0; b=0; } in 
-  for i = 0 to rows-1 do
+  for i = 0 to rows - 1 do
     for j = 0 to columns - 1 do
-      let coord_iter = get i j m in
-      let julia = julia_color (fst coord_iter) (snd coord_iter) in 
-      Rgb24.set orig_image i j julia;
+      Rgb24.set orig_image j i (f (get i j m));
     done;
   done;
   let fin_image = Images.Rgb24 orig_image
