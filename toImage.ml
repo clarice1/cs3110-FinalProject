8458b7a784c97098e38b8fe4dbabfbee60ac7a05
Pedro Suau <pps33@cornell.edu>
2020-10-23 11:49:34 -0400

    some more fixes

--- toImage.ml
+++ toImage.ml
@@ -3,14 +3,14 @@ open Images
 
 type col = R | B | G
 
-let julia_color iter col coordinate = 
+let julia_color iter of_color coordinate = 
   let black : Color.rgb = {r = 0; g = 0; b = 0} in
   if fst coordinate = None then black
   else begin
     let coord_iter = match fst coordinate with 
       | None -> 0
       | Some x -> x in 
-    match col with
+    match of_color with
     | R -> let red_col : Color.rgb = 
              {b = 0; r = 255 - ((coord_iter) * 255 / iter); g = 0;} in red_col
     | B -> let blue_col : Color.rgb = 
@@ -19,14 +19,15 @@ let julia_color iter col coordinate =
              {b = 0; r = 0; g = 255 - ((coord_iter) * 255 / iter);} in green_col
   end
 
-let colorize iter col (f : (int -> col -> int option * Complex.t -> Color.rgb)) m =
+let colorize iter of_color 
+    (f : (int -> col -> int option * Complex.t -> Color.rgb)) m =
   let rows = rows m
   and columns = columns m in
   let orig_image = Rgb24.make columns rows {r = 0; g = 0; b = 0; } in 
   for i = 0 to rows - 1 do
     for j = 0 to columns - 1 do
       let coord = get i j m in
-      Rgb24.set orig_image j i (f iter col coord);
+      Rgb24.set orig_image j i (f iter of_color coord);
     done;
   done;
   Images.Rgb24 orig_image
