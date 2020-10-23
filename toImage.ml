dc47f55e57541133f3c18176a41d0caf84e73c0c
Pedro Suau <pps33@cornell.edu>
2020-10-23 10:48:02 -0400

    implemented julia_color

--- toImage.ml
+++ toImage.ml
@@ -1,7 +1,25 @@
 open Matrix
 open Images
-let julia_color iter coordinate = 
-  failwith "unimplemented"
+
+type col = R | B | G
+
+let julia_color iter col coordinate = 
+  let black : Color.rgb = {r = 0; g = 0; b = 0} in
+  if fst coordinate = None then black
+  else begin
+    let coord_iter = match fst coordinate with 
+      | None -> 0
+      | Some x -> x in 
+    match col with
+    | R -> let red_col : Color.rgb = 
+             {b = 0; r = 255 - ((coord_iter) * 255 / iter); g = 0;} in red_col
+    | B -> let blue_col : Color.rgb = 
+             {b = 255 - ((coord_iter) * 255 / iter); r = 0; g = 0;} in blue_col
+    | G -> let green_col : Color.rgb = 
+             {b = 0; r = 0; g = 255 - ((coord_iter) * 255 / iter);} in green_col
+  end
+
+
 
 let colorize f m =
   let rows = rows m
--- toImage.mli
+++ toImage.mli
@@ -5,21 +5,24 @@
    matrix determining the particular color in the corresponding region on .bmp.
    It further handles the colorization of a Julia Set.
 *)
-
-
 open Images
 
-(** [julia_color iter coordinate] is the color of a particular complex
+(** [ col ] is the type invariant that represents the color desired by the 
+    client for the fractal. If the col were to be R, then the fractal would be 
+    colored with shades of red. This is also true for B (Blue) and G (Green). *)
+type col = R | B | G
+
+(** [julia_color iter of_color coordinate ] is the color of a particular complex
     [coordinate] in the matrix, having applied the function for this particular 
     Julia Set [iter] times. N.B. [coordinate] is a pair containing the final 
     value option after applying the function [iter] times (should it converge)
     as well as the complex coordinate *)
-val julia_color : int -> (int option * Complex.t) -> Color.rgb
+val julia_color : int -> col -> (int option * Complex.t) -> Color.rgb
 
 (** [colorize f m ] is the .bmp image of matrix [m] with function [f] applied to
     each complex coordinate to determine the color of that corresponding region
     of the image *)
-val colorize : ((int option * 'a) -> Color.rgb) -> 
+val colorize : (int -> col -> (int option * Complex.t) -> Color.rgb) -> 
   (int option * 'a) Matrix.t -> Images.t
 
 
