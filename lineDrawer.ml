c115c6dab31ad756bfac29fd72213d38e5d97f68
Nathaniel Bannister <njb226@cornell.edu>
2020-11-10 19:58:17 -0500

    Implemented zoom, back, quit in keyboard controls

--- lineDrawer.ml
+++ lineDrawer.ml
@@ -19,16 +19,23 @@ type state = {
   col : Graphics.color
 }
 
-let recompute s = 
+exception Q
+
+exception Z
+
+let rec_im f col iter ll ur color = 
   Graphics.set_color Graphics.white;
-  Graphics.fill_rect 0 0 (Graphics.size_x ()) (Graphics.size_y ());
-  Graphics.set_color s.col;
-  let s' = {s with width = Graphics.size_x (); height = Graphics.size_y ()} in
+  let (width, height) = Graphics.size_x (), Graphics.size_y () in 
+  Graphics.fill_rect 0 0 width height;
+  Graphics.set_color col;
   let beginning_matrix = 
-    Matrix.cx_init s.ll_c s.ur_c s'.height s'.width in
-  let final_matrix = Matrix.iterate_with_stop_2 s'.fb s.iter beginning_matrix in
-  let im = ToImage.colorize s.color final_matrix |> Graphic_image.of_image in
-  {s' with im = im}
+    Matrix.cx_init ll ur height width in
+  let final_matrix = Matrix.iterate_with_stop_2 f iter beginning_matrix in
+  ToImage.colorize color final_matrix |> Graphic_image.of_image 
+
+let recompute s = 
+  let im = rec_im s.fb s.col s.iter s.ll_c s.ur_c s.color in 
+  {s with width = Graphics.size_x (); height = Graphics.size_y (); im = im}
 
 
 (**[is_in_window s z] is [true] if and only if [z] is in bounds for the image*)
@@ -44,11 +51,13 @@ let cx_of_coord {width; height; ll_c; ur_c}
      im = (ur_c.im -. ll_c.im)/. 
           (float_of_int (height)) *. (float_of_int y)}
 
-let string_of_coord s p = 
-  let z = cx_of_coord s p in 
+let string_of_complex (z : Complex.t) = 
   string_of_float z.re ^ " + " ^ string_of_float z.im ^ "i"
 
-let redraw s col = 
+let string_of_coord s p = 
+  cx_of_coord s p |> string_of_complex
+
+let redraw s = 
   Graphics.draw_image s.im 0 0; 
   let (mp1, mp2) as mp = Graphics.mouse_pos () in
   let str = string_of_coord s mp in 
@@ -58,7 +67,7 @@ let redraw s col =
   Graphics.moveto 0 0;
   Graphics.set_color Graphics.black;
   Graphics.draw_string str;
-  Graphics.set_color col;
+  Graphics.set_color s.col;
   Graphics.moveto mp1 mp2
 
 (**[coord_of_cx s z] is the coordinate corrsponding to [z]*)
@@ -69,20 +78,23 @@ let coord_of_cx
   let dy = float_of_int (height) /. (ur_c.im -. ll_c.im) *. (im -. ll_c.im) in
   (int_of_float dx, int_of_float dy)
 
-let rec go s color click button = 
-  if Graphics.key_pressed () then button (Graphics.read_key ()) else
+let clicker click s mp = 
+  try if Graphics.button_down () then click (cx_of_coord s mp)
+  with 
+  | Z -> ()
+
+let rec go s (click : Complex.t -> unit) button = 
+  key_reader s click button;
   if Graphics.size_x () <> s.width || Graphics.size_y () <> s.height then
-    let s' = recompute s in 
-    redraw s' color;
-    go s' color click button
+    resize s click button 
   else
     let (mp1, mp2) as mp = Graphics.mouse_pos () in
-    if Graphics.button_down () then click (cx_of_coord s mp);
+    clicker click s mp;
     if mp = s.started_drawing then (*User has not moved mouse *)
       match s.last_point with 
       | None -> 
         Graphics.moveto mp1 mp2;
-        go {s with last_point = Some (cx_of_coord s mp)}  color click button
+        go {s with last_point = Some (cx_of_coord s mp)} click button
       | Some x -> 
         let fx = s.f (cx_of_coord s mp) x in
         if is_in_window s fx then
@@ -90,12 +102,49 @@ let rec go s color click button =
           Graphics.lineto x1 x2; 
           Unix.sleepf 0.2;
         else ();
-        go {s with last_point = Some fx}  color click button
+        go {s with last_point = Some fx} click button
     else (
-      redraw s color;
-      go {s with last_point = None; started_drawing = mp} color click button)
+      redraw s;
+      go {s with last_point = None; started_drawing = mp} click button)
 
-let start_with_bonus ll_c ur_c col fb color f iter click button im = 
+and resize s click button = 
+  let s' = recompute s in 
+  redraw s';
+  go s' click button
+
+and zoom factor s = 
+  let center = Graphics.mouse_pos () |> cx_of_coord s in 
+  let x_step = (s.ur_c.re -. s.ll_c.re) /. (2. *. factor) in
+  let y_step = (s.ur_c.im -. s.ll_c.im) /. (2. *. factor) in 
+  {s with ll_c = {re = center.re -. x_step; im = center.im -. y_step};
+          ur_c = {re = center.re +. x_step; im = center.im +. y_step}} 
+  |> recompute 
+
+and key_reader s click button = 
+  if Graphics.key_pressed () then
+    begin
+      match Graphics.read_key () with 
+      | 'q' -> raise Q
+      | 'z' -> raise Z
+      | 'c' -> start_with_bonus_state (zoom 2. s) click button
+      | 'e' -> start_with_bonus_state 
+                 (recompute {s with iter = s.iter * 2}) click button
+      | x -> let str = Char.escaped x in 
+        begin
+          match float_of_string_opt str with 
+          | Some factor when factor > 0. -> 
+            start_with_bonus_state (zoom factor s) click button
+          | _ -> button x
+        end
+    end
+
+and start_with_bonus_state s click button = 
+  try 
+    redraw s;
+    go s click button
+  with | Z -> ()
+
+let start_with_bonus_aux ll_c ur_c col fb color f iter click button im = 
   Graphics.set_color col;
   let width = Graphics.size_x () in 
   let height = Graphics.size_y () in
@@ -104,8 +153,12 @@ let start_with_bonus ll_c ur_c col fb color f iter click button im =
            last_point = None; im; width;
            color; f; 
            fb; iter; height; col} in
-  redraw s col;
-  go s col click button
+  start_with_bonus_state s click button
+
+let start_with_bonus ll_c ur_c col fb color f iter click button im = 
+  try start_with_bonus_aux ll_c ur_c col fb color f iter click button im with
+  | Q | Z -> ()
+
 
 let start ll_cx ur_cx c fb fc f iter im = 
   start_with_bonus ll_cx ur_cx c (fun x y -> fb y) fc (fun x y -> f y) iter 
--- lineDrawer.mli
+++ lineDrawer.mli
@@ -4,17 +4,16 @@
    the complex values corresponding to the lower left
    and upper right corners respectively. The graphics window should be
    already initialized to the desired ratio, but this function will draw
-   the image and resize if the window is resized.*)
+   the image and resize if the window is resized. [c] should be the 
+   desired color for lines. *)
 val start : Complex.t -> Complex.t -> Graphics.color -> 
   (Complex.t -> Complex.t option) -> ((int option * Complex.t) -> Color.rgb) ->
   (Complex.t -> Complex.t) -> int -> Graphics.image -> unit
 
 (**[start_with_bonus] is like [start], but the user may also specify the 
    drawing color, what happens when the mouse is clicked, 
-   depending on the complex value clicked, as well as what happens on keyboard 
-   input depending on the char inputted. 
-   The function may also depend on the initial value.
-   Customize the drawing color.*)
+   depending on the complex value clicked, and add additional key bindings.
+   The function may also depend on the initial value.*)
 val start_with_bonus : Complex.t -> Complex.t -> Graphics.color -> 
   (Complex.t -> Complex.t -> Complex.t option) -> 
   ((int option * Complex.t) -> Color.rgb) ->
