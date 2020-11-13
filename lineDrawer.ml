1ea0949921b973dc8bc6eb644e57e1e8ac0debfb
Nathaniel Bannister <njb226@cornell.edu>
2020-11-12 20:30:25 -0500

    Add more specifications for helper functions

--- lineDrawer.ml
+++ lineDrawer.ml
@@ -1,9 +1,19 @@
-(**A state consists of the lower left corner of the window, 
-   the upper right corner of the window, 
-   the complex number in the lower left corner,
+(**A state consists of the complex number in the lower left corner,
    the complex number in the upper right corner,
    where the current drawing started from,
-   and where the line drawn ends as a complex number*)
+   where the current line drawn ends as a complex number,
+   the image currently displayed in the graphics window,
+   the width and height of the image, 
+   the function to pass into [Matrix.iterate_with_stop_2] 
+   when recomputing is necessary (eg when zooming),
+   the function that computes where the line should next go,
+   the function that colors points corresponding to the iterations and
+   final value,
+   the number of iterations in the current image,
+   the drawing color,
+   the function activated when a point on the image is clicked, 
+   the function giving additional key bindings,
+   and the state to return to if the user wishes to redo*)
 type state = {
   ll_c : Complex.t;
   ur_c : Complex.t;
@@ -22,10 +32,14 @@ type state = {
   mutable redo : state option
 }
 
+(**The exception raised when ['q'] is pressed on the keyboard*)
 exception Q
 
+(**The exception raised when ['z'] is pressed on the keyboard containing the 
+   state when the exception was raised*)
 exception Z of state
 
+(**[rec_im] recomutes the image corresponding to the inputted functions*)
 let rec_im f col iter ll ur color = 
   Graphics.set_color Graphics.white;
   let (width, height) = Graphics.size_x (), Graphics.size_y () in 
@@ -36,6 +50,8 @@ let rec_im f col iter ll ur color =
   let final_matrix = Matrix.iterate_with_stop_2 f iter beginning_matrix in
   ToImage.colorize (color iter) final_matrix |> Graphic_image.of_image 
 
+(**[recompute s] is the state with image recomputed according to the other
+   parameters in [s]*)
 let recompute s = 
   let im = rec_im s.fb s.col s.iter s.ll_c s.ur_c s.color in 
   {s with width = Graphics.size_x (); height = Graphics.size_y (); im = im}
@@ -54,12 +70,16 @@ let cx_of_coord {width; height; ll_c; ur_c}
      im = (ur_c.im -. ll_c.im)/. 
           (float_of_int (height)) *. (float_of_int y)}
 
+(**[string_of_complex z] is the string representation of [z]*)
 let string_of_complex (z : Complex.t) = 
   string_of_float z.re ^ " + " ^ string_of_float z.im ^ "i"
 
+(**[string_of_coord s p] represents the complex value at coordinate [p]*)
 let string_of_coord s p = 
   cx_of_coord s p |> string_of_complex
 
+(**[redraw s] redraws the image stored in state [s], also updating the
+   complex value in the bottom left corner*)
 let redraw s = 
   Graphics.draw_image s.im 0 0; 
   let (mp1, mp2) as mp = Graphics.mouse_pos () in
@@ -87,10 +107,13 @@ let catch_z s e1 =
     s.redo <- Some s';
     redraw s
 
+(**[clicker] reacts to a user clicking on the mouse corresponding to 
+   the complex value clicked and [s.click]*)
 let clicker (s : state) (mp : int * int) : unit = 
   (fun () -> if Graphics.button_down () then s.click (cx_of_coord s mp)) 
   |> catch_z s
 
+(**[go] is the main operation unit of [lineDrawer]*)
 let rec go s  = 
   key_reader s;
   if Graphics.size_x () <> s.width || Graphics.size_y () <> s.height then
@@ -115,11 +138,14 @@ let rec go s  =
       redraw s;
       go {s with last_point = None; started_drawing = mp})
 
+(**[resize s] starts the graphic with the new window size*)
 and resize s= 
   let s' = recompute s in 
   redraw s';
   go s'
 
+(**[zoom factor s] is the state corresponding to zooming in by a factor of 
+   [factor] centered at the current mouse position*)
 and zoom factor s = 
   let center = Graphics.mouse_pos () |> cx_of_coord s in 
   let x_step = (s.ur_c.re -. s.ll_c.re) /. (2. *. factor) in
@@ -129,17 +155,34 @@ and zoom factor s =
           last_point = None} 
   |> recompute 
 
+(**[e s] is the function that activates when ['e'] is clicked in state [s].
+   Currently increases iterations by a factor of 2.*)
 and e s = 
   (fun () -> recompute {s with iter = s.iter * 2; last_point = None} |> 
              start_with_bonus_state) |> (catch_z s)
 
+(**[c s] is the function that activates when ['c'] is clicked in state [s].
+   Currently zooms by a factor of 20 centered at current mouse position*)
 and c s = (fun () -> zoom 20. s |> start_with_bonus_state) |> catch_z s
 
+(**[y s] is the function that activates when ['y'] is clicked in state [s].
+   Currently undoes most recent click of ['z']*)
 and y s = 
   match s.redo with 
   | None -> () 
   | Some s' -> (fun () -> start_with_bonus_state_with_redo s') |> catch_z s
 
+(**[int_press s x] is the functions that activates when a key corresponding
+   to a digit '1' - '9' is pressed. Currently zooms in by that amount,
+   centered on the current mouse position*)
+and int_press s x = 
+  (fun () -> 
+     let factor = float_of_int (int_of_char x - int_of_char '0') in 
+     start_with_bonus_state (zoom factor s))
+  |> catch_z s
+
+(**[key_reader] reads user input from the keyboard and reacts according
+   to which key was pressed.*)
 and key_reader s = 
   if Graphics.key_pressed () then
     begin
@@ -149,14 +192,8 @@ and key_reader s =
       | 'c' -> c s
       | 'e' -> e s
       | 'y' -> y s
-      | x -> 
-        (fun () -> let str = Char.escaped x in 
-          begin
-            match float_of_string_opt str with 
-            | Some factor when factor > 0. -> 
-              start_with_bonus_state (zoom factor s) 
-            | _ -> s.button x
-          end) |> catch_z s
+      | x when '1' <= x && x <= '9' -> int_press s x
+      | x -> s.button x
     end
 
 and start_with_bonus_state_with_redo (s : state) : unit =
